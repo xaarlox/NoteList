56649b1e889e151605eeb2e12d0be42a234213b1
Valeriia Fedorenko <xaarlox@gmail.com>
2025-04-13 18:36:37 +0300

    added use cases

--- app/src/main/java/com/xaarlox/notelist/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/xaarlox/notelist/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,5 @@
 package com.xaarlox.notelist.feature_note.domain.use_case
 
 data class NoteUseCases(
-    val getNotes: GetNotes,
-    val deleteNote: DeleteNote
+    val getNotes: GetNotes, val deleteNote: DeleteNote
 )
