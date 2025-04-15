3166f6fb9cb0e9ea0c226a19c638983bd1099374
Valeriia Fedorenko <xaarlox@gmail.com>
2025-04-15 17:37:35 +0300

    created NotesViewModel

---
+++ app/src/main/java/com/xaarlox/notelist/feature_note/presentation/notes/NotesViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.xaarlox.notelist.feature_note.presentation.notes
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.xaarlox.notelist.feature_note.domain.use_case.NoteUseCases
+import javax.inject.Inject
+
+class NotesViewModelFactory @Inject constructor(
+    private val noteUseCases: NoteUseCases
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NotesViewModel::class.java)) {
+            return NotesViewModel(noteUseCases) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
