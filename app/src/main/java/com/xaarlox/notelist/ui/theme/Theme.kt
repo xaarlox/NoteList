f91b95f7c9505aeb548b25a6c530394218e538a9
Valeriia Fedorenko <xaarlox@gmail.com>
2025-04-15 21:24:50 +0300

    modified ui (color and theme)

--- app/src/main/java/com/xaarlox/notelist/ui/theme/Theme.kt
+++ app/src/main/java/com/xaarlox/notelist/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.xaarlox.notelist.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,12 +8,15 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.White,
+    background = Color.DarkGray,
+    onBackground = Color.White,
+    surface = //LightBlue,
+    //onSurface = DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -53,6 +55,7 @@ fun NoteListTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
